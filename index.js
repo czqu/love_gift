b2feafe1b58d189bc37e4bf9048de9f7d0ee8578
czqu <ryan.h.qin@outlook.com>
2020-08-23 22:55:20 +0800

    000

--- index.js
+++ index.js
@@ -57,9 +57,9 @@
 
   $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>跳过动画</a>\n    <a style='color: #fff' href='index.html'>重新播放</a>\n    </div>");
 
-  commentTime = 40;
+  commentTime = 30;
 
-  codeTime = 0.1;
+  codeTime = 1;
 
   $(document).on('ready page:load', function() {
     return $('#skipAnimation').click(function() {
